d7f5021df855aab3e0c3bf1126cf988711d698a7
Florian Gilde <fgilde@gmail.com>
2026-07-18 22:36:32 +0200

    feat: REST endpoints wiring services

--- src/AspireUI.Server/Program.cs
+++ src/AspireUI.Server/Program.cs
@@ -1,6 +1,13 @@
+using AspireUI.Server.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();       // serves built SPA from wwwroot (Task 10 copies it here)
+app.MapStackEndpoints();
+app.MapFallbackToFile("index.html");
 
 app.Run();
+
+public partial class Program { } // expose for WebApplicationFactory
